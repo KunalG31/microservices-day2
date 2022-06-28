5cdb3f50aa9a5b4f9b4362b6a640fd0b370454b0
Kunal Gupta <kunalgupta31@gmail.com>
2022-06-28 15:33:51 -0400

    Before last break on Day 2

---
+++ services/CoursesSolution/CoursesApi/Domain/Course.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CoursesApi.Domain
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("title")]
+        public string Title { get; set; } = String.Empty;
+
+        [BsonElement("category")]
+        public string Category { get; set; } = String.Empty;
+
+        [BsonElement("positionInCategory")]
+        public int PositionInCategory { get; set; }
+
+        [BsonElement("numberOfDays")]
+        public int NumberOfDays { get; set; }
+
+    }
+}
